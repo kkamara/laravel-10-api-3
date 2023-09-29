284ec8a1d5d79891dddcf016dcf90d66fd9023e6
kkamara <kamaracomputers@gmail.com>
2023-09-29 17:44:33 +0100

    Create Resources With POST Requests

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -39,19 +39,15 @@ class CustomerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(
+            Customer::create(
+                $request->all()
+            )
+        );
     }
 
     /**
