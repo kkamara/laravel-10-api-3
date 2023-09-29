53c14672af3f40fcec5dfa6d8c07783c44e246de
kkamara <kamaracomputers@gmail.com>
2023-09-29 18:15:24 +0100

    Protecting Routes With Sanctum

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(
     [
         "prefix" => "v1",
-        "namespace" => "App\Http\Controllers\API\V1"
+        "namespace" => "App\Http\Controllers\API\V1",
+        "middleware" => "auth:sanctum"
     ],
     function() {
         Route::apiResource(
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +29,49 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get("/setup", function() {
+    $credentials = [
+        "email" => "admin@admin.com",
+        "password" => "password"
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        $user = new User();
+
+        $user->first_name = "Admin";
+        $user->last_name = "";
+        $user->email = $credentials["email"];
+        $user->password = Hash::make($credentials["password"]);
+
+        $user->save();
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken(
+                "admin-token",
+                ["create", "update", "delete"]
+            );
+            $updateToken = $user->createToken(
+                "update-token",
+                ["create", "update"]
+            );
+            $basicToken = $user->createToken(
+                "basic-token"
+            );
+
+            return [
+                "admin" => $adminToken
+                    ->plainTextToken,
+                "update" => $updateToken
+                    ->plainTextToken,
+                "basic" => $basicToken
+                    ->plainTextToken,
+            ];
+        }
+    }
 });
 
 require __DIR__.'/auth.php';
